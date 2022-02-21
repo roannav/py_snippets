28f17aad4b680c3e53f5c52ee30d549ed3218c7f
Roanna Victorio <roannav@users.noreply.github.com>
2022-02-20 23:38:00 -0800

    add 3 date formats

--- regex_date.py
+++ regex_date.py
@@ -3,20 +3,26 @@
 # eg July 2, 1999
 
 # Detect and then convert these formats:
+# Month_name DD, YYYY
+# Month_name DD YYYY
 # DD Month_name YYYY
 # YYYY MM DD
+# MM DD YYYY
 
 import re
 
 dateRegex = re.compile(r'''
-    (\d{4}\s+\d{1,2}\s+\d{1,2}) |            # YYYY MM DD
-    (\d{1,2}\s+[ADFJMSON][a-z]+\.?\s+\d{4})     # DD Month_name YYYY
+    ([ADFJMSON][a-z]+\.?\s+\d{1,2},?\s+\d{4}) |  # Month_name DD, YYYY
+                                                 # Month_name DD YYYY
+    (\d{1,2}\s+[ADFJMSON][a-z]+\.?\s+\d{4}) |    # DD Month_name YYYY
+    (\d{4}\s+\d{1,2}\s+\d{1,2}) |                # YYYY MM DD
+    (\d{1,2}\s+\d{1,2}\s+\d{4})                  # MM DD YYYY
     ''', re.VERBOSE)
 
-match = dateRegex.search("2022 1 1")
+match = dateRegex.search("January 1, 2022")
 print(match.group())
 
-match = dateRegex.search("2022 01 01")
+match = dateRegex.search("January 1 2022")
 print(match.group())
 
 match = dateRegex.search("1 January 2022")
@@ -27,3 +33,16 @@ print(match.group())
 
 match = dateRegex.search("01 Jan. 2022")
 print(match.group())
+
+match = dateRegex.search("2022 1 1")
+print(match.group())
+
+match = dateRegex.search("2022 01 01")
+print(match.group())
+
+match = dateRegex.search("1 1 2022")
+print(match.group())
+
+match = dateRegex.search("01 01 2022")
+print(match.group())
+
