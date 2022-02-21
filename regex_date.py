8f27562f1efb430d3f6765e2b4fc1f5399537d6c
Roanna Victorio <roannav@users.noreply.github.com>
2022-02-21 09:14:46 -0800

    allow hyphen to separate date components

--- regex_date.py
+++ regex_date.py
@@ -7,20 +7,20 @@
 # Month_name DD YYYY
 # DD Month_name YYYY
 # YYYY MM DD
-# YYYY/MM/DD
+# YYYY/MM/DD or YYYY-MM-DD
 # MM DD YYYY
-# MM/DD/YYYY
+# MM/DD/YYYY or MM-DD-YYYY
 
 import re
 
 dateRegex = re.compile(r'''
-    ([ADFJMSON][a-z]+\.?\s+\d{1,2},?\s+\d{4}) |  # Month_name DD, YYYY
-                                                 # Month_name DD YYYY
-    (\d{1,2}\s+[ADFJMSON][a-z]+\.?\s+\d{4}) |    # DD Month_name YYYY
-    (\d{4}\s+\d{1,2}\s+\d{1,2}) |                # YYYY MM DD
-    (\d{4}\s*/?\s*\d{1,2}\s*/?\s*\d{1,2}) |      # YYYY/MM/DD
-    (\d{1,2}\s+\d{1,2}\s+\d{4}) |                # MM DD YYYY
-    (\d{1,2}\s*/?\s*\d{1,2}\s*/?\s*\d{4})        # MM/DD/YYYY
+    ([ADFJMSON][a-z]+\.?\s+\d{1,2},?\s+\d{4}) |    # Month_name DD, YYYY or
+                                                   # Month_name DD YYYY
+    (\d{1,2}\s+[ADFJMSON][a-z]+\.?\s+\d{4}) |      # DD Month_name YYYY
+    (\d{4}\s+\d{1,2}\s+\d{1,2}) |                  # YYYY MM DD
+    (\d{4}\s*[/-]?\s*\d{1,2}\s*[/-]?\s*\d{1,2}) |  # YYYY/MM/DD or YYYY-MM-DD
+    (\d{1,2}\s+\d{1,2}\s+\d{4}) |                  # MM DD YYYY
+    (\d{1,2}\s*[/-]?\s*\d{1,2}\s*[/-]?\s*\d{4})    # MM/DD/YYYY or MM-DD-YYYY
     ''', re.VERBOSE)
 
 match = dateRegex.search("January 1, 2022")
@@ -60,5 +60,11 @@ match = dateRegex.search("1/1/2022")
 print(match.group())
 
 match = dateRegex.search("01 / 01 / 2022")
+print(match.group())
+
+match = dateRegex.search("2022-01-01")
+print(match.group())
+
+match = dateRegex.search("1-1-2022")
 print(match.group())
 
