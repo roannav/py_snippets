b7832847fa5ac824c7435428adf59a08d2dc8fb4
Roanna Victorio <roannav@users.noreply.github.com>
2022-03-21 13:45:02 -0700

    removing spaces and tabs

---
+++ string_replace.py
@@ -0,0 +1,13 @@
+# remove the spaces
+print("skdjf dkjfls sdfjsd dskfj".replace(' ', ''))
+
+# remove the tabs
+print("This is a tab here\t\tksdjfdjf".replace('\t', ''))
+
+# NOTE: remove the tabs using a space as the first param. THIS DOES NOT WORK.  
+print("This is a tab here\t\tksdjfdjf".replace(' ', ''))
+
+# NOTE: I'm not going to make an example where I type 'tab' key,
+# but then when the editor saves the tab, the tabs are replaced with spaces,
+# because obviously, they are just spaces now. (I didn't just make that 
+# mistake right now. LOL)
